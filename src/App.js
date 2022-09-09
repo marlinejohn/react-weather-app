f5ff73100331b073a5d692c6509837be6f9d2887
marlinejohn <marlinejohn@gmail.com>
2022-09-09 20:01:24 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was created by Marline John and is{" "}
-        <a
-          href="https://github.com/marlinejohn/weather-app-react"
-          target="_blank "
-          rel="noreferrer"
-        >
-          Open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was created by Marline John and is{" "}
+          <a
+            href="https://github.com/marlinejohn/weather-app-react"
+            target="_blank "
+            rel="noreferrer"
+          >
+            Open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
