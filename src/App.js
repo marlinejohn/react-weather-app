32f63f828509cc099c68e2c7bf88f18b5494947e
marlinejohn <marlinejohn@gmail.com>
2022-09-09 11:32:23 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was created by Marline John and is{" "}
+        <a
+          href="https://github.com/marlinejohn/weather-app-react"
+          target="_blank"
+        >
+          Open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
