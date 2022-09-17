836ea4daaa2a5e2e79216170c7e525567c5fcea6
marlinejohn <marlinejohn@gmail.com>
2022-09-17 21:34:22 +0200

    Updated icon codes.

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -13,14 +13,14 @@ export default function WeatherIcon(props) {
     "04n": "CLOUDY",
     "09d": "RAIN",
     "09n": "RAIN",
-    "010d": "RAIN",
-    "010n": "RAIN",
-    "011d": "RAIN",
-    "011n": "RAIN",
-    "013d": "RAIN",
-    "013n": "RAIN",
-    "050d": "FOG",
-    "050n": "FOG",
+    "10d": "RAIN",
+    "10n": "RAIN",
+    "11d": "RAIN",
+    "11n": "RAIN",
+    "13d": "RAIN",
+    "13n": "RAIN",
+    "50d": "FOG",
+    "50n": "FOG",
   };
   // console.log("Mappinng", codeMapping[props.code]);
 
